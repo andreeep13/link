330a6d38c095b5b3b72abdf629a30006eaa5f8d9
joxiplayer <joxiplayer.id@gmail.com>
2021-12-20 09:01:35 +0700

    first commit

---
+++ Settings/assets/js/login-2.js
@@ -0,0 +1,127 @@
+// Your web app's Firebase configuration (ENTER YOUR FIREBASE CONFIGURATION DETAILS)
+var firebaseConfig = {
+    apiKey: "AIzaSyBdNz6lLVkSCnNroDw-CCTmiZuOistyOPk",
+    authDomain: "blog-andre-id.firebaseapp.com",
+    databaseURL: "https://blog-andre-id-default-rtdb.firebaseio.com",
+    projectId: "blog-andre-id",
+    storageBucket: "blog-andre-id.appspot.com",
+    messagingSenderId: "521308543059",
+    appId: "1:521308543059:web:d2c8eb6cbe5011966b0ea6",
+    measurementId: "G-KZVJB7W181",
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    var form = document.querySelector('#loginForm');
+    var r_form = document.querySelector('#registerForm');
+    var reset_form = document.querySelector('#resetForm');
+    var message = document.querySelector('#messageDiv');
+    var message_value = document.querySelector('.message');
+    var sign_out = document.querySelector("#signOut");
+    
+    // check if user is logged in or not
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            if(window.location.pathname != '/../daftar/index.html'){
+                window.location = '../daftar/index.html';
+            }
+        } else {
+            if(window.location.pathname === '/../daftar/index.html'){
+                window.location = 'index.html';
+            }
+        }
+    });
+    
+    // user login
+    if(form){
+        form.addEventListener('submit', function(e) {
+            e.preventDefault();
+            let email = form.email.value;
+            let password = form.password.value;
+        
+            firebase.auth().signInWithEmailAndPassword(email, password)
+            .then((userCredential) => {
+                window.location = '../daftar/index.html';
+            })
+            .catch((error) => {
+                message.style.display = 'block';
+                message_value.innerText = error.message;
+                setTimeout(function(){
+                    message.style.display = 'none';
+                }, 3000);
+            });
+        })
+    }
+    
+    // user register
+    if(r_form){
+        r_form.addEventListener('submit', function(e) {
+            e.preventDefault();
+            let email = r_form.email.value;
+            let password = r_form.password.value;
+        
+            firebase.auth().createUserWithEmailAndPassword(email, password)
+            .then((userCredential) => {
+                window.location = '../daftar/index.html';
+            })
+            .catch((error) => {
+                message.style.display = 'block';
+                message_value.innerText = error.message;
+                setTimeout(function(){
+                    message.style.display = 'none';
+                }, 3000);
+            });
+        })
+    }
+    
+    // password reset 
+    if(reset_form){
+        reset_form.addEventListener('submit', function(e) {
+            e.preventDefault();
+            let email = reset_form.email.value;
+        
+            firebase.auth().sendPasswordResetEmail(email)
+            .then((userCredential) => {
+                message.style.display = 'block';
+                message_value.innerText = 'Email has been send!';
+                
+            })
+            .catch((error) => {
+                message.style.display = 'block';
+                message_value.innerText = error.message;
+                setTimeout(function(){
+                    message.style.display = 'none';
+                }, 3000);
+            });
+        })
+    }
+    
+    var provider = new firebase.auth.GoogleAuthProvider();
+    var login = document.querySelector('.login');
+    
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        // console.log(user);
+        window.location = '../daftar/index.html';
+      }
+    });
+    
+    login.addEventListener('click', (e) => {
+      firebase.auth()
+        .signInWithPopup(provider)
+        .then((result) => {
+          window.location = '../daftar/index.html'
+        }).catch((error) => {
+          console.log(error);
+        });
+    })
+    
+    // sign out  
+    if(sign_out){
+        sign_out.addEventListener('click', function(e) {
+            firebase.auth().signOut().then(() => {
+                window.location = 'index.html';
+            }).catch((error) => {
+            // An error happened.
+            });
+        })
+    }
